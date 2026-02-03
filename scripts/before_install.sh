7906c94907c67fbb4916fc4163b4f943753a6cbb
gopineedi chaithanya <chaithanyagopineedi@gmail.com>
2026-02-03 22:10:58 +0530

    Update before_install.sh

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -3,16 +3,22 @@ set -e
 
 echo "===== BEFORE INSTALL ====="
 
-# Create app directory if not exists
+# Update system
+sudo apt update -y
+
+# Install Node.js & npm
+sudo apt install -y nodejs npm
+
+# Install PM2 globally
+sudo npm install -g pm2
+
+# Create app directory
 mkdir -p /home/ubuntu/myapp
 
-# Clean old files (optional but recommended)
+# Clean old deployment
 rm -rf /home/ubuntu/myapp/*
 
-echo "Directory prepared"
-
-# Make sure scripts will be executable after extract
+# Make all other scripts executable
 chmod +x /home/ubuntu/myapp/scripts/*.sh || true
 
 echo "Before install completed"
-
