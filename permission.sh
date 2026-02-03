11b0aa063287cab1a9ccbd54e0476e6a98171472
gopineedi chaithanya <chaithanyagopineedi@gmail.com>
2026-02-03 21:24:44 +0530

    Create permission.sh

---
+++ permission.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# Make all other scripts executable
+chmod +x /home/ubuntu/myapp/scripts/*.sh
+
+# Your existing before install commands
+echo "Running before_install tasks..."
+# Add any other commands you need here
